18391063d739cc04899530a41d03514777863732
Jack Ling <j.lingyx@gmail.com>
2018-09-07 23:41:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array, element){
 }
 
 function addElementToBeginningOfArray(element, array){
-  var newArray = chocolateBars.unshift(element, array)
+  var newArray = chocolateBars.unshift(array, element)
 }
 
 function destructivelyAddElementToBeginningOfArray(){
