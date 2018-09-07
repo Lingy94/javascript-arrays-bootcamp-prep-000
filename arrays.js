328fc86f1ce76402565bc9514118754491a09457
Jack Ling <j.lingyx@gmail.com>
2018-09-07 23:25:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,5 +3,5 @@ var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 var arrayNew = []
 
 function addElementToBeginningOfArray(array, element){
-  arrayNew = [element. ...array]
+  arrayNew = [element, ...array]
 }
